4cc3214dc532a6b58bc5dca2ced9d77b785e7940
javad101 <javad.amindehgha1@gmail.com>
2022-06-25 06:51:24 -0700

    Updates

---
+++ precache-manifest.a414ecb3b16ec130e4170b8be98947c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81aef6255cc21bfbc2c5067a85c67571",
+    "url": "/color/index.html"
+  },
+  {
+    "revision": "317b2ed16cbe41dabef6",
+    "url": "/color/static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/color/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/color/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "317b2ed16cbe41dabef6",
+    "url": "/color/static/js/main.4c9bb60d.chunk.js"
+  },
+  {
+    "revision": "8d093fefa50f65ee0ec5",
+    "url": "/color/static/js/runtime-main.60abaf5e.js"
+  }
+]);
